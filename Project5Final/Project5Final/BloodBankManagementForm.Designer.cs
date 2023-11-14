2adbfa8f849616f5443ed4158a535e07d116d73b
DESKTOP-AIO2UAN\rushilreddie <rushilreddy.js@gmail.com>
2023-11-13 21:53:13 -0600

    Added Main Form Files

---
+++ Project5Final/Project5Final/BloodBankManagementForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Project5Final
+{
+    partial class BloodBankManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.donorManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searhForDonorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stockManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.addDonorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateDonorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteDonorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewAllDonorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.byAddressToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bloodGroupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.increaseDecreaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.donorManagementToolStripMenuItem,
+            this.searhForDonorToolStripMenuItem,
+            this.stockManagementToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1070, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // donorManagementToolStripMenuItem
+            // 
+            this.donorManagementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addDonorToolStripMenuItem,
+            this.updateDonorToolStripMenuItem,
+            this.deleteDonorToolStripMenuItem,
+            this.viewAllDonorsToolStripMenuItem});
+            this.donorManagementToolStripMenuItem.Name = "donorManagementToolStripMenuItem";
+            this.donorManagementToolStripMenuItem.Size = new System.Drawing.Size(189, 29);
+            this.donorManagementToolStripMenuItem.Text = "&Donor Management";
+            // 
+            // searhForDonorToolStripMenuItem
+            // 
+            this.searhForDonorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.byAddressToolStripMenuItem,
+            this.bloodGroupToolStripMenuItem});
+            this.searhForDonorToolStripMenuItem.Name = "searhForDonorToolStripMenuItem";
+            this.searhForDonorToolStripMenuItem.Size = new System.Drawing.Size(167, 29);
+            this.searhForDonorToolStripMenuItem.Text = "S&earch For Donor";
+            // 
+            // stockManagementToolStripMenuItem
+            // 
+            this.stockManagementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.increaseDecreaseToolStripMenuItem,
+            this.detailsToolStripMenuItem});
+            this.stockManagementToolStripMenuItem.Name = "stockManagementToolStripMenuItem";
+            this.stockManagementToolStripMenuItem.Size = new System.Drawing.Size(181, 29);
+            this.stockManagementToolStripMenuItem.Text = "Stock &Management";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(55, 29);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.exitToolStripMenuItem1.Text = "E&xit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // addDonorToolStripMenuItem
+            // 
+            this.addDonorToolStripMenuItem.Name = "addDonorToolStripMenuItem";
+            this.addDonorToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.addDonorToolStripMenuItem.Text = "&Add New Donor";
+            // 
+            // updateDonorToolStripMenuItem
+            // 
+            this.updateDonorToolStripMenuItem.Name = "updateDonorToolStripMenuItem";
+            this.updateDonorToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.updateDonorToolStripMenuItem.Text = "&Update Donor";
+            // 
+            // deleteDonorToolStripMenuItem
+            // 
+            this.deleteDonorToolStripMenuItem.Name = "deleteDonorToolStripMenuItem";
+            this.deleteDonorToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.deleteDonorToolStripMenuItem.Text = "&Delete Donor";
+            // 
+            // viewAllDonorsToolStripMenuItem
+            // 
+            this.viewAllDonorsToolStripMenuItem.Name = "viewAllDonorsToolStripMenuItem";
+            this.viewAllDonorsToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.viewAllDonorsToolStripMenuItem.Text = "&View All Donors";
+            // 
+            // byAddressToolStripMenuItem
+            // 
+            this.byAddressToolStripMenuItem.Name = "byAddressToolStripMenuItem";
+            this.byAddressToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.byAddressToolStripMenuItem.Text = "&Location";
+            // 
+            // bloodGroupToolStripMenuItem
+            // 
+            this.bloodGroupToolStripMenuItem.Name = "bloodGroupToolStripMenuItem";
+            this.bloodGroupToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.bloodGroupToolStripMenuItem.Text = "&Blood Group";
+            // 
+            // increaseDecreaseToolStripMenuItem
+            // 
+            this.increaseDecreaseToolStripMenuItem.Name = "increaseDecreaseToolStripMenuItem";
+            this.increaseDecreaseToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.increaseDecreaseToolStripMenuItem.Text = "&Increase / Decrease";
+            // 
+            // detailsToolStripMenuItem
+            // 
+            this.detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
+            this.detailsToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.detailsToolStripMenuItem.Text = "D&etails";
+            // 
+            // BloodBankManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project5Final.Properties.Resources.BackgroundImage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1070, 605);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "BloodBankManagementForm";
+            this.Text = "Blood Bank Management System";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem donorManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addDonorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateDonorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteDonorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewAllDonorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searhForDonorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stockManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem byAddressToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bloodGroupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem increaseDecreaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detailsToolStripMenuItem;
+    }
+}
+
---
+++ Project5Final/Project5Final/BloodBankManagementForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project5Final
+{
+    public partial class BloodBankManagementForm : Form
+    {
+        public BloodBankManagementForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
